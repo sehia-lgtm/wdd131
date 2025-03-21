3f2e1395011423285920503b6ee117def6487bf7
sehia-lgtm <arthursehi95@gmail.com>
2025-03-21 21:05:57 +0000

    Added comments to the code

--- week01/script.js
+++ week01/script.js
@@ -7,5 +7,7 @@ function createParagraph() {
   const buttons = document.querySelectorAll("button");
   
   for (const button of buttons) {
+    // I am using the addEventListener method to listen for the click event on the button element.
+    /* Sehi Arthur*/
     button.addEventListener("click", createParagraph);
   }
