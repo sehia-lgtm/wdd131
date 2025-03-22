e4b84d83b09988c8ca8929ab8ce6ef892370beaf
sehia-lgtm <arthursehi95@gmail.com>
2025-03-22 23:50:22 +0000

    First part completed

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+// Dynamically update the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Dynamically update the last modified date
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
