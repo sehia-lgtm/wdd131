e3caffae6dfb3f263ff0727e513f92a52c093274
sehia-lgtm <arthursehi95@gmail.com>
2025-03-29 22:07:43 +0000

    Done

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -4,10 +4,10 @@ const areaOutput = document.querySelector('area');
 let area = 0;
 const PI = 3.14159;
 
-const radius = 10;
+let radius = 10;
 area = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
 radius = 20;
 area = PI * radius * radius;
