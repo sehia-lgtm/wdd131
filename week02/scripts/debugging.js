a2d1bbab3121f839b0600c0e450addee6e9d78ec
sehia-lgtm <arthursehi95@gmail.com>
2025-03-29 22:16:08 +0000

    Weird

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -6,8 +6,8 @@ const PI = 3.14159;
 
 let radius = 10;
 area = PI * radius * radius;
-radiusOutput.textContent = radius;
-areaOutput.textContent = area;
+radiusOutput = radius;
+areaOutput = area;
 
 radius = 20;
 area = PI * radius * radius;
