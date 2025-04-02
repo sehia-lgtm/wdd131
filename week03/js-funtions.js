0da4cc6c6d03574c9fb465332a18de52d972806b
sehia-lgtm <arthursehi95@gmail.com>
2025-04-02 15:34:58 +0000

    js functions

---
+++ week03/js-funtions.js
@@ -0,0 +1,14 @@
+function fullName(first, last) {
+    return `${first} ${last}` ;
+}
+
+const fullName = function (first, last) {
+     return `${first} ${last}`;
+}
+
+const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+document.querySelector('#fullName').innerHTML = fullName(firstName, lastName);
+
+
+
